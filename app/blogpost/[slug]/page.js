a5a40eb48329e8f2e298ced967059cd218bdb906
Anas Hussain <hussainanas68@gmail.com>
2025-09-18 00:02:08 +0500

    done-wameq

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -22,8 +22,6 @@ export default async function BlogPostPage({ params }) {
   const imageUrl = cover?.url
     ? `http://localhost:1337${cover.url}`
     : "https://via.placeholder.com/800x400.png?text=No+Image";
-
-  // Fetch all articles for related blogs
   // const relatedRes = await fetch(
   //   "http://localhost:1337/api/articles?populate=*&pagination[pageSize]=6"
   // );
